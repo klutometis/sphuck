57f6c1938061437d4e4c0667dd0151b56eb223cb
Peter Danenberg <pcd@wikitex.org>
2009-04-29 03:31:56 -0700

    bizarre string stream hack

---
+++ string-reader.php
@@ -0,0 +1,25 @@
+<?php
+
+class StringReader {
+  public $string;
+  public $length;
+  public $index;
+  function stream_open($path, $mode, $options, &$opened_path) {
+    $this->string = parse_url($path, PHP_URL_HOST);
+    $this->length = strlen($this->string);
+    return true;
+  }
+  function stream_read($count) {
+    $substr = substr($this->string, $this->index, $count);
+    $this->index += $count;
+    return $substr;
+  }
+  function stream_tell() {
+    return $this->index;
+  }
+  function stream_eof() {
+    return $this->index >= $this->length;
+  }
+  }
+
+stream_wrapper_register('str', 'StringReader');
